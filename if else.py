5ac66f66ed43e0d74757492362620a3151164fe8
honeyakshat999 <37788408+honeyakshat999@users.noreply.github.com>
2019-08-26 01:48:51 +0530

    use and comarision of strings

---
+++ if else.py
@@ -0,0 +1,17 @@
+marks=input("plz enter the marks\n")
+
+if marks >50:
+    print "you did well"
+elif marks <20:
+    print "you got failed"
+else :
+    print "you did very bad"
+
+name = raw_input("enter name :")
+#for taking strings as input we use raw_input() function
+
+#name='akshat'
+if name == "akshat":
+    print "you are authorised"
+else:
+    print "you are not authorised"
